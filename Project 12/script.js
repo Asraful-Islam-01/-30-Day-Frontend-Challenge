5d977d0b4081fa17c5584411c0ac113e79eced60
Asraful Islam <asrafulislam.12967@gmail.com>
2025-10-02 00:41:47 +0600

    added some javaScript to the codebase

---
+++ Project 12/script.js
@@ -0,0 +1,11 @@
+function updateClock() {
+    const now = new date();
+
+    //get time components
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    //add leading zeros
+    hours = hours < 10; ?
+}
