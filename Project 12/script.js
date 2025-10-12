1cd5d1fbd49e8c1bc1ed3e749182f7dcbb75f15a
Asraful Islam <asrafulislam.12967@gmail.com>
2025-10-12 18:29:51 +0600

    initial commit for project 13

--- Project 12/script.js
+++ Project 12/script.js
@@ -7,5 +7,17 @@ function updateClock() {
     let seconds = now.getSeconds();
 
     //add leading zeros
-    hours = hours < 10; ?
+    hours = hours < 10; ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    // Format time string with blinking clones
+    const timeString = `${hours}<span class= "colon">:</span>${minutes}<span class="colon">:</span>${seconds}`;
+
+    //Update time display
+    document.getElementById("time").innerHTML = timeString;
+    // get date components
+    const days = []
+        
 }
+
