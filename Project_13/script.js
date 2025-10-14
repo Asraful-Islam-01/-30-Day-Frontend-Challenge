08f87f9e7d70c4240fb8b55489eb21cc6abc5780
Asraful Islam <asrafulislam.12967@gmail.com>
2025-10-15 01:01:57 +0600

    added half of js code to the project

---
+++ Project_13/script.js
@@ -0,0 +1,10 @@
+let currentSlide = 0;
+const image = document.querySelectorAll(".carousel-image");
+const indicators = document.querySelectorAll(".indicator");
+const titles = {
+  "Random Image 1",
+  "Random Image 2",
+  "Random Image 3",
+  "Random Image 4",
+  "Random Image 5"
+}
