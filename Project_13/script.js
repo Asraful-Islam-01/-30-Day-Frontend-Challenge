965f7335497a626bdabb4b233f436a60ab5adfad
Asraful Islam <asrafulislam.12967@gmail.com>
2025-10-17 15:11:45 +0600

    added js to the project 13

--- Project_13/script.js
+++ Project_13/script.js
@@ -8,3 +8,22 @@ const titles = {
   "Random Image 4",
   "Random Image 5"
 }
+
+function showSlide(n) {
+  if (n >= image.length) currentSlide = 0;
+  if (n < 0) currentSlide = images.length - 1;
+
+  images.forEach((img) => img.classList.remove("active"));
+  indicators.forEach((ind) => ind.classList.remove("active"));
+
+  images[currentSlide].classList.add("active");
+  indicators[currentSlide].classList.add("active");
+
+  document.getElementById("imageTitle").textContent = titles[currentSlide];
+  document.getElementById("counter").textContent = `${currentSlide + 1} / ${images.length}`;
+}
+
+function changeSlide(direction) {
+  currentSlide += direction;
+  showSlide(currentSlide);  
+} 
