1daf7091f0ce484dbcddbf6cf3c2117da2d97e8a
Asraful Islam <asrafulislam.12967@gmail.com>
2025-10-20 16:28:36 +0600

    This is the initial commit for the project 14

---
+++ Project 14/script.js
@@ -0,0 +1,9 @@
+function changeColor() {
+    //Generate random value for red, green and blue (0-255)
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() *256);
+    const b = Math.floor(Math.random() * 256);
+
+    // Create RGB color string
+    const color = "rgb(" + r + ", " +
+}
